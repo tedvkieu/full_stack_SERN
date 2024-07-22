b7f36c8cdb264983ae393f989d52d329af08ff01
tedvkieu <dvkieu165@gmail.com>
2024-07-22 20:53:56 +0700

    #20 Tạo tables, thêm dữ liệu database với Sequelize CLI - Cấu hình database cho Node.JS

---
+++ back_end/src/seeders/20240722134718-demo-user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Users', [
+            {
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'tedvkieu@gmail.com',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    },
+};
